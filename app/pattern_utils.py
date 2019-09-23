1eb14b5c1ab9f7087188c680ffce4cace68b8379
Dewmal <dewmalnilanka@gmail.com>
2019-09-24 03:13:00 +0530

    Now init project

---
+++ app/pattern_utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import argrelextrema
+
+
+def peak_detect(price, order):
+    # Find relative extrema
+    max_idx = list(argrelextrema(price, np.greater, order=order)[0])
+    min_idx = list(argrelextrema(price, np.less, order=order)[0])
+    # Concat extrema
+    idx = max_idx + min_idx + [len(price) - 1]
+    idx.sort()
+
+    current_idx = idx[-5:]
+
+    start_idx = min(current_idx)
+    end_idx = max(current_idx)
+
+    current_pat = price.values[current_idx]
+
+    return idx, current_pat, start_idx, end_idx
